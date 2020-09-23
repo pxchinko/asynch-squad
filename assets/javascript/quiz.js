4573da952843870f105ae2334f2b4a4837434217
Sarah Massie <smassie@my365.bellevue.edu>
2020-09-23 15:16:49 -0700

    master

---
+++ assets/javascript/quiz.js
@@ -0,0 +1,115 @@
+var json = {
+  title: "JavaScript Quiz",
+  showProgressBar: "bottom",
+  showTimerPanel: "top",
+  maxTimeToFinishPage: 60,
+  maxTimeToFinish: 600,
+  firstPageIsStarted: true,
+  startSurveyText: "Start Quiz",
+  pages: [
+  {
+    question: [
+      {
+      type: "html"
+      }
+    ]
+  },
+  {
+    question: "What is the correct way to write a JavaScript array?",
+    answers: {
+      a: 'arrayName = [item1, item2, item3]',
+      b: 'var arrayName = item1, item2, item3',
+      c: 'var arrayName = \"item1, item2, item3\"',
+      d: 'var arrayName = [item1, item2, item3]'
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: 'How does JavaScript store dates in a date object?',
+    answers: {
+      a: 'The number of milliseconds since January 1st, 1970.',
+      b: 'The number of days since January 1st, 1900.',
+      c: 'The number of seconds since Netscape\'s public stock offering.',
+      d: 'None of the above.',
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'How do you comment in JavaScript?',
+    answers: {
+      a: '<Comment Here></Comment>',
+      b: '<!--Comment Here-->',
+      c: '/Comment Here/',
+      d: '//Comment Here',
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: 'How do you define a function in JavaScript?',
+    answers: {
+      a: 'Function myFunction() { //code here }',
+      b: 'run myFunction { // code here }',
+      c: 'function myFunction() { // code here }',
+      d: 'Functions are defined in a separate file.',
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'What is the expected output of: \n&ltscript type = \"text/javascript\"&gt \nx = 4 + \"4\";\ndocument.write(x);\n&lt/script&gt',
+    answers: {
+      a: '44',
+      b: '8',
+      c: '4',
+      d: 'Error output.',
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'What is the correct JavaScript syntax to write \"Hello World\"?',
+    answers: {
+      a: 'System.out.printIn(\"Hello World\")',
+      b: 'printIn(\"Hello World\")',
+      c: 'document.write(\"Hello World\")',
+      d: 'response.write(\"Hello World\")',
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Where is the JavaScript placed inside an HTML document or page?',
+    answers: {
+      a: 'JavaScript cannot be placed inside an HTML document.',
+      b: 'In the &ltbody&gt and &lthead&gt sections.',
+      c: 'Only in the &lthead&gt section.',
+      d: 'Only in the &ltbody&gt section.',
+    },
+    correctAnswer: 'd'
+  },
+  {
+    question: 'What is meant by the \"this\" keyword in JavaScript?',
+    answers: {
+      a: 'It refers to the current object.',
+      b: 'It refers to the previous object.',
+      c: 'It is a variable which contains a value.',
+      d: 'None of the above.',
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'JavaScript is a case sensitive language.',
+    answers: {
+      a: 'True',
+      b: 'False'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Which group of array methods are used in JavaScript?',
+    answers: {
+      a: 'splice(), concat(), delete(), toString()',
+      b: 'toString(), bubbleSort(), order(), quicksort()',
+      c: '&ltarray&gt, &ltdeque&gt, &ltset&gt, &ltvector&gt',
+      d: 'SELECT, BREACH, FROM, INSERT',
+    },
+    correctAnswer: 'a'
+  },
+]
